526730c09de45beff2690e84d1ee8cff7ecf1bac
byeol3325 <byeol3325@dgist.ac.kr>
2025-09-17 20:42:09 +0900

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,9 +35,31 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      {/* 전체 레이아웃: 왼쪽 사이드바 + 오른쪽 본문 */}
+      <div style={{ display: 'flex', alignItems: 'flex-start', gap: '2rem', padding: '2rem' }}>
+        
+        {/* 왼쪽 사이드 프로필 */}
+        <aside style={{ flex: '0 0 250px', borderRight: '1px solid #ddd', paddingRight: '1rem' }}>
+          <img 
+            src="static/img/moka.jpg" 
+            alt="My Profile" 
+            style={{ width: '100%', borderRadius: '50%', marginBottom: '1rem' }} 
+          />
+          <h3>{siteConfig.title}</h3>
+          <p>{siteConfig.tagline}</p>
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li><a href="https://github.com/byeol3325" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/sungho-moon-byeol3325/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="/docs/intro">Docs</a></li>
+            <li><a href="/blog">Blog</a></li>
+          </ul>
+        </aside>
+
+        {/* 오른쪽 본문 */}
+        <main style={{ flex: 1 }}>
+          <HomepageFeatures />
+        </main>
+      </div>
     </Layout>
   );
 }
